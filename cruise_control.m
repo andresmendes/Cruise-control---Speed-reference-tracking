be383c1f7f1c36568a99eacfa6795c22313ca52c
André de Souza Mendes <asmendes@fei.edu.br>
2021-05-03 10:37:14 -0300

    Update cruise_control.m

--- cruise_control.m
+++ cruise_control.m
@@ -1,6 +1,6 @@
 %% Cruise control - Speed reference tracking
-% Animation of a vehicle with cruise control and varying speed reference.
-%
+% Simulation and animation of a vehicle with cruise control and varying
+% speed reference.
 %%
 
 clear ; close all ; clc
